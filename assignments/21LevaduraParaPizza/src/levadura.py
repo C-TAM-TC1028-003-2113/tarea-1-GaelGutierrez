97006b7ee27750ae203a81a5e2cc09ecaa29835e
GaelGutierrez <89654741+GaelGutierrez@users.noreply.github.com>
2021-08-29 22:43:29 -0500

    Add files via upload

---
+++ assignments/21LevaduraParaPizza/src/levadura.py
@@ -0,0 +1,9 @@
+def main():
+    # escribe tu código abajo de esta línea
+   harina = float(input('Dame la harina en gramos: '))
+   final = harina / 50
+   print('Necesitas estos gramos de levadura: ', final)
+
+
+if __name__ == '__main__':
+    main()
